f0f9f30360d464768fd01e0c6be717354d4bda64
mbrickenworthe <adamwinters456@gmail.com>
2015-10-27 10:08:07 -0400

    Changing the database for the heroku app

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151019020350) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text     "content"
     t.integer  "post_id"
